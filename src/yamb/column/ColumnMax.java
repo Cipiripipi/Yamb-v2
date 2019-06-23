4facbe891bd835e034884d2a36131a337a30d2df
Cipiripipi <44265813+Cipiripipi@users.noreply.github.com>
2019-06-23 03:08:41 +0200

    .

--- src/yamb/column/ColumnMax.java
+++ src/yamb/column/ColumnMax.java
@@ -15,68 +15,67 @@ public class ColumnMax extends Column
 		super(dicesCanvas, roll, chk);
 		this.setImageForColumn("Kockice/ColumnMax.png", "M");
 		
-		b1.setOnMouseReleased(e -> {
+		b1.setOnMouseClicked(e -> {
 			if (CheckResult.checkNumber(dicesCanvas, 1) != 5)
 				b1.setText("0");
 		});
 		
-		b2.setOnMouseReleased(e -> {
+		b2.setOnMouseClicked(e -> {
 			if (CheckResult.checkNumber(dicesCanvas, 2) != 10)
 				b2.setText("0");
 		});
 		
-		b3.setOnMouseReleased(e -> {
+		b3.setOnMouseClicked(e -> {
 			if (CheckResult.checkNumber(dicesCanvas, 3) != 15)
 				b3.setText("0");
 		});
 		
-		b4.setOnMouseReleased(e -> {
+		b4.setOnMouseClicked(e -> {
 			if (CheckResult.checkNumber(dicesCanvas, 4) != 20)
 				b4.setText("0");
 		});
 		
-		b5.setOnMouseReleased(e -> {
+		b5.setOnMouseClicked(e -> {
 			if (CheckResult.checkNumber(dicesCanvas, 5) != 25)
 				b5.setText("0");
 		});
 		
-		b6.setOnMouseReleased(e -> {
+		b6.setOnMouseClicked(e -> {
 			if (CheckResult.checkNumber(dicesCanvas, 6) != 30)
 				b6.setText("0");
 		});
 		
-		kenta.setOnMouseReleased(e -> {
+		kenta.setOnMouseClicked(e -> {
 			if (kenta.getText().equalsIgnoreCase("56") || kenta.getText().equalsIgnoreCase("46"))
 				kenta.setText("0");
-			
 		});
 		
-		triling.setOnMouseReleased(e -> {
+		triling.setOnMouseClicked(e -> {
 			if (CheckResult.checkTriling(dicesCanvas) != 38)
 				triling.setText("0");
 		});
 		
-		full.setOnMouseReleased(e -> {
+		full.setOnMouseClicked(e -> {
 			if (CheckResult.checkFull(dicesCanvas) != 58)
 				full.setText("0");
 		});
 		
-		poker.setOnMouseReleased(e -> {
+		poker.setOnMouseClicked(e -> {
 			if (CheckResult.checkPoker(dicesCanvas) != 64)
 				poker.setText("0");
 		});
 		
-		yamb.setOnMouseReleased(e -> {
+		yamb.setOnMouseClicked(e -> {
 			if (CheckResult.checkYamb(dicesCanvas) != 80)
 				yamb.setText("0");
 		});
 		
-		max.setOnMouseReleased(e -> {
+		max.setOnMouseClicked(e -> {
 			if (max.getText().equalsIgnoreCase("30") == false)
 				max.setText("0");
 		});
 		
-		min.setOnMouseReleased(e -> {
+		min.setOnMouseClicked(e -> {
 			if (min.getText().equals("5") == false)
 				min.setText("0");
 		});
